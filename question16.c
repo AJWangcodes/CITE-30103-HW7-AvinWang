5d8afe4fc90e143fb97d871c9913a6a4bd5668d1
Avin Wang <ajwang@lovelace>
2025-12-01 17:51:19 -0600

    Q16: Define a struct Student with fields for name, roll, and marks. Then print information

---
+++ question16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct Student{
+	char name[40];
+	int roll;
+	float marks;
+};
+int main(void){
+	struct Student s;
+	scanf("%39s %d %f", s.name, &s.roll, &s.marks);
+	printf("Name : %s\n", s.name);
+        printf("Roll : %d\n", s.roll);
+        printf("Marks : %.2f\n", s.marks);
+	return 0;
+	
+}
