c86e2a2dad7bc47ee443b2ecbc255e5f2697d137
Avin Wang <ajwang@lovelace>
2025-12-02 12:50:37 -0600

    Q24: function pointer calling square(int) and print result

---
+++ question24.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int square(int x){
+	return x*x;
+}
+
+int main(void){
+	int n;
+	scanf("%d", &n);
+	int (*fp)(int)=square;
+	int result = fp(n);
+	printf("Square = %d\n", result);
+	return 0;
+}
